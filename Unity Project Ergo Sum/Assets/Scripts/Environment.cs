6d90b522b092d0d25572cc137f89cc7b343648b2
Ryan <rmccaffrey3@gatech.edu>
2014-01-25 16:16:51 -0500

    no message

--- Unity Project Ergo Sum/Assets/Scripts/Environment.cs
+++ Unity Project Ergo Sum/Assets/Scripts/Environment.cs
@@ -7,4 +7,7 @@ public interface Environment {
 	
 	// Update is called once per frame
 	void Update (CharacterController character);
+	void FixedUpdate(CharacterController character);
+	void onEnter(CharacterController character);
+	void onExit(CharacterController character);
 }
