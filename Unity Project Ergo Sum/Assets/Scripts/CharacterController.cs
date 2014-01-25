38dd092134c686ebf07e848f6a54827d765158bc
Ryan <rmccaffrey3@gatech.edu>
2014-01-25 03:28:23 -0500

    interfaces

---
+++ Unity Project Ergo Sum/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class CharacterController : MonoBehaviour {
+	public int health;
+	public string name;
+	public int deathTimer;
+	public ArrayList<Status> currentStatuses;
+	public Environment environment;
+	// Use this for initialization
+	void Start ();
+	
+	// Update is called once per frame
+	void Update ();
+}
---
+++ Unity Project Ergo Sum/Assets/Scripts/Status.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface Status {
+
+	// Use this for initialization
+	void otherMove(CharacterController character);
+	void jump(CharacterController character);
+	void left(CharacterController character);
+	void right(CharacterController character);
+	void up(CharacterController character);
+	void down(CharacterController character);
+	void fixedUpdate(CharacterController character);
+	void onEnter(CharacterController character);
+
+}
