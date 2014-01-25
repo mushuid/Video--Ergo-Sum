d61a3ea52248035e966edb9e79fa393fde917cfa
shigamori <shigamori.james@gmail.com>
2014-01-25 14:04:48 -0500

    Update Status.cs

--- Unity Project Ergo Sum/Assets/Scripts/Status.cs
+++ Unity Project Ergo Sum/Assets/Scripts/Status.cs
@@ -6,8 +6,8 @@ public interface Status {
 	// Use this for initialization
 	void otherMove(CharacterController character);
 	void jump(CharacterController character);
-	void left(CharacterController character);
-	void right(CharacterController character);
+	bool left(CharacterController character);
+	bool right(CharacterController character);
 	void up(CharacterController character);
 	void down(CharacterController character);
 	void fixedUpdate(CharacterController character);
