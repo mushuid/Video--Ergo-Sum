0ad417f5c08db987a13bedbe7f245f1184696ce9
James Neumeister <shigamori.james@gmail.com>
2014-01-25 16:22:26 -0500

    Added Rain environment script

--- Unity Project Ergo Sum/Assets/Scripts/Environment State Scripts/Rain.cs
+++ Unity Project Ergo Sum/Assets/Scripts/Environment State Scripts/Rain.cs
@@ -1,13 +1,21 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
-public interface Environment {
+public class Rain:Environment {
 
-
+	rainTimer = 0;
 	
 	// Update is called once per frame
 	void Update (CharacterController character);
-	void FixedUpdate(CharacterController character);
+	void FixedUpdate(CharacterController character)
+	{
+		rainTimer++
+		if(RainTimer >= 60)
+			{
+				deathTimer++;
+				rainTimer = 0;
+			}
+	}
 	void onEnter(CharacterController character);
 	void onExit(CharacterController character);
 	bool isDamaging(CharacterController character);
