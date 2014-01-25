bd2de568728c3df90186a2718a21bbd20d3e66e0
Ryan <rmccaffrey3@gatech.edu>
2014-01-25 16:07:59 -0500

    no message

--- Unity Project Ergo Sum/Assets/Scripts/CharacterController.cs
+++ Unity Project Ergo Sum/Assets/Scripts/CharacterController.cs
@@ -5,6 +5,7 @@ public abstract class CharacterController : MonoBehaviour {
 	public int health;
 	public string name;
 	public int deathTimer;
+	public boolean onGround;
 	public ArrayList<Status> currentStatuses;
 	public Environment environment;
 	// Use this for initialization
